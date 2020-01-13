10efea16607b2a2b7e7651156997e7afa827290b
TestAccount-bit <gerhugwsif@gmail.com>
2020-01-13 13:06:19 +0100

    GameControllerEndGame

---
+++ Go/Server/src/main/test/gameLogic/goban/GobanTest.java
@@ -0,0 +1,50 @@
+package gameLogic.goban;
+
+import static org.junit.Assert.*;
+
+public class GobanTest {
+
+    @org.junit.Test
+    public void checkIfMovePossible() {
+    }
+
+    @org.junit.Test
+    public void putStone() {
+    }
+
+    @org.junit.Test
+    public void getFieldType() {
+    }
+
+    @org.junit.Test
+    public void getField() {
+    }
+
+    @org.junit.Test
+    public void update() {
+    }
+
+    @org.junit.Test
+    public void removeStones() {
+    }
+
+    @org.junit.Test
+    public void setTerritories() {
+    }
+
+    @org.junit.Test
+    public void getWhiteTerritory() {
+    }
+
+    @org.junit.Test
+    public void getBlackTerritory() {
+    }
+
+    @org.junit.Test
+    public void getWhiteCaptured() {
+    }
+
+    @org.junit.Test
+    public void getBlackCaptured() {
+    }
+}
