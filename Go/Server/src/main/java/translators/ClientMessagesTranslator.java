c352651b31993b081649ba074d6e77171bd7db75
TestAccount-bit <gerhugwsif@gmail.com>
2020-01-13 01:53:48 +0100

    ClientMessagesTranslator and GamePlayTranslator classes extensions

--- Go/Server/src/main/java/translators/ClientMessagesTranslator.java
+++ Go/Server/src/main/java/translators/ClientMessagesTranslator.java
@@ -1,10 +1,14 @@
 package translators;
 
+import exceptions.EmptyNameException;
+import exceptions.NameContainsSpaceException;
 import gameLogic.Game;
 import server.ClientHandler;
 
-public class ClientMessagesTranslator {
+import java.util.Objects;
 
+
+public class ClientMessagesTranslator {
     private final ClientHandler clientHandler;
     private final Game game;
 
@@ -13,14 +17,85 @@ public class ClientMessagesTranslator {
         game = clientHandler.getGame();
     }
 
+
     public void processIncomingMessage(String message) {
+        String response = "";
+        if (message.startsWith("CONNECTION OK")) {
+            clientHandler.send("SETNAME");
+            return;
+        } else if (message.startsWith("USERNAME")) {
+            try {
+                if (game.addPlayer(message.replaceFirst("USERNAME ", ""), clientHandler))
+                    response = "NAMEOK";
+                else
+                    response = "NAMETAKEN";
+            } catch (NameContainsSpaceException | EmptyNameException e) {
+                return;
+            }
+        } else if (message.startsWith("LIST")) {
+            response = "LIST " + getListOfNotInGamePlayers();
+        } else if (message.startsWith("OPPONENT")) {
+            message = message.replaceFirst("OPPONENT ", "");
+            if (game.inviteOpponent(message, clientHandler.getPlayer().getName())) return;
+            else response = "CHOOSEOPPONENTAGAIN " + getListOfNotInGamePlayers();
+        } else if (message.startsWith("INVAGREE")) {
+            message = message.replaceFirst("INVAGREE ", "");
+            if (game.chooseOpponent(message, clientHandler.getPlayer())) return;
+            else response = "CHOOSEOPPONENTAGAIN " + getListOfNotInGamePlayers();
+        } else if (message.startsWith("INVDECLINE")) {
+            message = message.replaceFirst("INVDECLINE ", "");
+            if (game.refuseInvitation(message)) return;
+            else response = "CHOOSEOPPONENTAGAIN " + getListOfNotInGamePlayers();
+        } else if (message.startsWith("MOVE")) {
+            String[] coords = message.replaceFirst("MOVE ", "").split(" ");
+            if (coords.length == 2) {
+                clientHandler.getPlayer().makeMove(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+                return;
+            } else if (coords[0].equals("PASS")) {
+                clientHandler.getPlayer().passMove();
+                return;
+            }
+        } else if (message.startsWith("SURRENDER ")) {
+            clientHandler.getPlayer().getGamePlay().endGame(clientHandler.getPlayer());
+            return;
+        } else if (message.startsWith("DEADSUGGESTION")) {
+            clientHandler.getPlayer().sendProposal(message);
+            return;
+        } else if (message.startsWith("TERRITORYSUGGESTION")) {
+            clientHandler.getPlayer().sendProposal(message);
+            return;
+        } else if (message.startsWith("ACCEPT")) {
+            clientHandler.getPlayer().acceptSuggestion();
+            return;
+        } else if (message.startsWith("DELETE ")) {
+            message = message.replaceFirst("DELETE ", "");
+            game.deletePlayer(game.getPlayerByName(message));
+            return;
+        } else if (message.startsWith("RESUME")) {
+            clientHandler.getPlayer().getGamePlay().resumeGame(clientHandler.getPlayer());
+        } else response = "UNKNOWNCOMMAND";
+        clientHandler.send(response);
     }
 
-    public void sendInvitation(String player) {
 
+    private String getListOfNotInGamePlayers() {
+        StringBuilder b = new StringBuilder();
+        String myName = clientHandler.getPlayer().getName();
+        for (String name : game.getNotInGamePlayersNames()) {
+            if (!Objects.equals(name, myName)) b.append(name).append(" ");
+        }
+
+        return b.toString();
+    }
+
+
+    public void sendInvitation(String player) {
+        clientHandler.send("INVITATIONFROM " + player);
     }
 
     public void sendRefusal() {
-
+        clientHandler.send("DECLINED");
     }
+
+
 }
